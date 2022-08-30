0dc362f6183d76d204709eab2b7daa541950e67f
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2022-08-30 10:09:41 +0300

    change task 6

--- js/task-06.js
+++ js/task-06.js
@@ -4,13 +4,13 @@ const refs = {
 
 refs.input.addEventListener("blur", onBlur);
 
-const length = refs.input.dataset.length;
+const length = Number(refs.input.dataset.length);
 
 function onBlur(event) {
   let eventLength = event.target.value.length;
   refs.input.classList.remove(...refs.input.classList);
 
-  if (eventLength >= length) {
+  if (eventLength === length) {
     refs.input.classList.add("valid");
   } else {
     refs.input.classList.add("invalid");
